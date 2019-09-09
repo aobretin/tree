50e8a545af16aca8373e9e373b80e4d1232a1a45
Obretin Alexandru <alexandruobretin@ymail.com>
2019-09-09 11:48:43 +0300

    Update Filter-input.js

--- src/__tests__/Filter-input.js
+++ src/__tests__/Filter-input.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {render} from '@testing-library/react';
 
-import FilterInput from '../components/Filter-Input';
+import FilterInput from '../components/Filter-input';
 import {SearchTermProvider} from '../providers/Search-term-provider';
 
 it('renders without crashing', () => {
